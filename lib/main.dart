fb1fb9ca4f2eba0840c8e6077ff73340fc719f49
Andrioeffendi31 <andrioeffendi31@gmail.com>
2021-08-17 22:07:17 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Andrio Effendi',
       //this is dark theme
       theme: ThemeData.dark().copyWith(
         primaryColor: primaryColor,
